dee6240e37d4a80fa3237fcfa83db0096caf65d0
ImNotGabo <gabriel.duarte18@outlook.es>
2025-01-29 20:26:03 -0400

    Log Ironman power instead of the entire object

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import * as SuperHero from './classes/Hero';
 import powers from './data/power';
 
 const ironman = new SuperHero.Hero('Ironman', 1, 32);
-console.log(ironman);
+console.log(ironman.power);
 console.log(powers);
