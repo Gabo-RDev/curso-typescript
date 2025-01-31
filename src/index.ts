fbde386449d59146991296fd47c769f15bd65101
ImNotGabo <gabriel.duarte18@outlook.es>
2025-01-30 20:26:42 -0400

    Update index.ts to log the date from generic function instead of using toFixed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { printObject, genericfunction } from './generics/generics';
 
 console.log(genericfunction(3.1416).toFixed(2));
-// console.log(genericfunction(new Date()).toFixed(2));
+console.log(genericfunction(new Date()).getDate());
