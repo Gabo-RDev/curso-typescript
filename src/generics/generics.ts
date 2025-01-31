10fbe0b617d9deec95f8360bcc71fdaccf6574aa
ImNotGabo <gabriel.duarte18@outlook.es>
2025-01-30 20:26:45 -0400

    Enhance generics utility by updating genericfunction to use generics and adding genericFunctionArrow

--- src/generics/generics.ts
+++ src/generics/generics.ts
@@ -2,6 +2,8 @@ export const printObject = (argument: any) => {
 	console.log(argument);
 };
 
-export function genericfunction(argument: any) {
-  return argument
+export function genericfunction<T>(argument: T): T {
+	return argument;
 }
+
+export const genericFunctionArrow = <T>(arg: T) => arg;
