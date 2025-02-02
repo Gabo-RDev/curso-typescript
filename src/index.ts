3dc95b4d43a899ce640c52ab495f2ff11cf9c54f
ImNotGabo <gabriel.duarte18@outlook.es>
2025-02-01 23:32:42 -0400

    Add savePokemonToDB call for Charmander with a valid Pokémon ID

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ const charmander = new Pokemon('Charmander');
 // (Pokemon.prototype as any).customName = 'Pikachu'; // object is not extensible
 
 console.log(charmander);
+charmander.savePokemonToDB(900);
